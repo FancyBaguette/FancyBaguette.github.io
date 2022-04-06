cf10dfd8d21572cfdce3e7aa31a52c7d168bc48f
Fancy <maciekgplay@gmail.com>
2022-04-06 20:48:35 +0200

    scroll to top button

---
+++ main.js
@@ -0,0 +1,16 @@
+button = document.getElementById("go-to-top-btn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+function goToTop() {
+  document.body.scrollTop = 0;  // safari
+  document.documentElement.scrollTop = 0; // chrome, opera etc.
+} 
