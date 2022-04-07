2eddfb8142d67df04f582063d05be6b5aae4a956
Fancy <maciekgplay@gmail.com>
2022-04-07 13:09:51 +0200

    new f.e.m. challange

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     button.style.display = "block";
+    button.classList.add('animate__animated', 'animate__fadeInUp');
   } else {
-    button.style.display = "none";
+    button.style.display = 'none';
   }
 }
 
