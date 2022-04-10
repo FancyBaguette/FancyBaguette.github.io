4e30c8a8d97d06ed64037ce512307286a708c422
Fancy <maciekgplay@gmail.com>
2022-04-10 13:01:58 +0200

    image transitions

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// // //
 let lastModifiedDate = document.lastModified;
 document.getElementById('lastModifiedSpan').innerHTML='Site was last modified at '+lastModifiedDate+' (MM/DD/YYYY)';
+// // //
 
 button = document.getElementById("go-to-top-btn");
 
@@ -8,7 +10,6 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     button.style.display = "block";
-    button.classList.add('animate__animated', 'animate__fadeInUp');
   } else {
     button.style.display = 'none';
   }
